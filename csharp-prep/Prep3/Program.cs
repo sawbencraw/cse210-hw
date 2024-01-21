bddf116c111f184f18dc714be8a2cd3f34f8c2d0
Sawyer Crawley <crawleysawyer@gmail.com>
2024-01-20 22:50:50 -0700

    Completed prep 3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int myNumber = randomGenerator.Next(1, 201);
+
+        int guess = -1;
+        while (guess != myNumber)
+        {
+            Console.Write("Guess a number between 1 & 201! ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (myNumber > guess)
+            {
+                Console.WriteLine("Try again, but higher this time!");
+            }
+            else if (myNumber < guess)
+            {
+                Console.WriteLine("Try again, but lower this time!");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+
+        }                    
     }
 }
